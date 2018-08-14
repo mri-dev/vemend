256f2de3e9b1aa744099696063fc31ac5afded2a
mri-dev <pocokxtrame@gmail.com>
2018-08-14 17:03:40 +0200

    Save

--- site/application/views/v1.0/templates/programs_home.php
+++ site/application/views/v1.0/templates/programs_home.php
@@ -8,3 +8,23 @@
     </div>
   </div>
 </div>
+<div class="cont-holder">
+  <div class="wrapper">
+    <div class="image">
+      <img src="" alt="">
+    </div>
+    <div class="data">
+      <div class="wrapper">
+        <div class="badges">
+          
+        </div>
+        <div class="data-content">
+          <h3>Program címe</h3>
+          <div class="desc">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
