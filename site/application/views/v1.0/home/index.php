5881dc2296863c739753b421bb31bed94c64b9d8
mri-dev <pocokxtrame@gmail.com>
2018-08-15 12:22:59 +0200

    Save

--- site/application/views/v1.0/home/index.php
+++ site/application/views/v1.0/home/index.php
@@ -399,12 +399,67 @@
   <div class="content-wrapper">
     <div class="pw">
       <div class="szallasok">
-        <div class="flex">
-          <div class="szallas-kereso">
-            Szállás kereső
-          </div>
-          <div class="szallas-top">
-            Top szállás ajánlatok
+        <div class="holder">
+          <div class="flex">
+            <div class="szallas-kereso">
+              <div class="wrapper">
+                <form class="" action="/szallasok" method="get">
+                  <div class="head">
+                    <i class="fa fa-briefcase"></i> Szállás keresése
+                  </div>
+                  <div class="fcont">
+                    <div class="wrapper">
+                      <div class="inp">
+                        <div class="wrapper">
+                          <label for="szallas_erkezes">Érkezés</label>
+                          <input type="text" id="szallas_erkezes" name="erkezes" value="">
+                        </div>
+                      </div>
+                      <div class="inp">
+                        <div class="wrapper">
+                          <label for="szallas_tavozas">Távozás</label>
+                          <input type="text" id="szallas_tavozas" name="tavozas" value="">
+                        </div>
+                      </div>
+                      <div class="inp">
+                        <div class="wrapper">
+                          <label for="szallas_ellatas">Ellátás</label>
+                          <select class="" id="szallas_ellatas" name="ellatas">
+                            <option value="">bármely</option>
+                          </select>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="space"></div>
+                  <div class="fcont grey">
+                    <div class="wrapper">
+
+                    </div>
+                  </div>
+                  <div class="space"></div>
+                  <div class="foot">
+                    <div class="flex">
+                      <div class="opt">
+                        <div class="fcont grey">
+                          <div class="wrapper">
+                            <input type="checkbox" name="opt[kisallat]" id="opt_kisallat"> <label for="opt_kisallat">kisállat hozható</label>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="button">
+                        <button type="submit"><i class="fa fa-search"></i> keresés</button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div class="szallas-top">
+              <div class="wrapper">
+                Top szállás ajánlatok
+              </div>
+            </div>
           </div>
         </div>
       </div>
