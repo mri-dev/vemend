25a1aabadf5d23faaa3fc0e2c5925da11202294e
mri-dev <pocokxtrame@gmail.com>
2019-01-04 18:08:24 +0100

    Save

--- site/application/views/v1.0/templates/programs_home.php
+++ site/application/views/v1.0/templates/programs_home.php
@@ -8,38 +8,44 @@
     </div>
   </div>
 </div>
-<div class="cont-holder">
-  <div class="wrapper">
-    <div class="image image-abs-center autocorrett-height-by-width" data-image-ratio="4:3" data-image-under="398">
-      <img src="<?=($this->program->getImage(true))?$this->program->getImage(true):''?>" alt="<?php echo $this->program->getTitle(); ?>">
-    </div>
-    <div class="data">
-      <div class="wrapper">
-        <div class="napico">
-          <i class="fa fa-calendar"></i>
-        </div>
-        <div class="badges">
-          <?php if (!is_null($this->program->getIdopont())): ?>
-          <div class="badge-orange" title="Esemény ideje">
-            <i class="fa fa-clock-o"></i> <?php echo $this->program->getIdopont('Y.m.d. H:i'); ?>
+<?php if (!$this->program): ?>
+  <div class="no-programs">
+    <h3><i class="fa fa-bell-o"></i><br>Jelenleg nincs közelgő program.</h3>
+  </div>
+<?php else: ?>
+  <div class="cont-holder">
+    <div class="wrapper">
+      <div class="image image-abs-center autocorrett-height-by-width" data-image-ratio="4:3" data-image-under="398">
+        <img src="<?=($this->program->getImage(true))?$this->program->getImage(true):''?>" alt="<?php echo $this->program->getTitle(); ?>">
+      </div>
+      <div class="data">
+        <div class="wrapper">
+          <div class="napico">
+            <i class="fa fa-calendar"></i>
           </div>
-          <?php endif; ?>
-        </div>
-        <div class="data-content">
-          <h3><?php echo $this->program->getTitle(); ?></h3>
-          <div class="desc">
-            <?php echo $this->program->getDescription(); ?>
+          <div class="badges">
+            <?php if (!is_null($this->program->getIdopont())): ?>
+            <div class="badge-orange" title="Esemény ideje">
+              <i class="fa fa-clock-o"></i> <?php echo $this->program->getIdopont('Y.m.d. H:i'); ?>
+            </div>
+            <?php endif; ?>
           </div>
-        </div>
-        <div class="footer">
-          <div class="flex">
-            <div class="off"></div>
-            <div class="button">
-              <a href="<?php echo $this->program->getUrl(); ?>">Tovább</a>
+          <div class="data-content">
+            <h3><?php echo $this->program->getTitle(); ?></h3>
+            <div class="desc">
+              <?php echo $this->program->getDescription(); ?>
+            </div>
+          </div>
+          <div class="footer">
+            <div class="flex">
+              <div class="off"></div>
+              <div class="button">
+                <a href="<?php echo $this->program->getUrl(); ?>">Tovább</a>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-</div>
+<?php endif; ?>
